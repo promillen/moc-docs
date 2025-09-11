9e0cc11cd0f650dfaeb31fdf7592b0231a6009b2
Christoffer Kolbe Boye-Hansen <christoffer.kolbe@gmail.com>
2025-09-11 09:14:33 +0200

    Documented log view

--- pages/development/_meta.tsx
+++ pages/development/_meta.tsx
@@ -1,5 +1,7 @@
 export default {
   "firmware-guide": "Firmware Development",
   "local-testing": "Local Testing",
+  "websocket-debugging": "WebSocket Live Logs Debugging",
+  "database-troubleshooting": "Database Storage Troubleshooting",
   "deployment": "Deployment Procedures"
 }
